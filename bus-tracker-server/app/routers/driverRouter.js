f1ffba1f5119a75364d8324e4c057fdbebf1dc83
AndriiDronDiachuk <dyachuk.droid@gmail.com>
2018-03-11 15:03:22 +0200

    Added driverRouter.

--- bus-tracker-server/app/routers/driverRouter.js
+++ bus-tracker-server/app/routers/driverRouter.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const driverRouter = new express.Router();
+const userController = require('../controllers/userController');
+const responseErrorsHandler = require('../services/userResponceHandler');
+
+driverRouter.delete('/delete-user/:id', async (req, res) => {
+    const handlersResult = responseErrorsHandler('delete-user', await userController.deleteUser(req));
+    res.status(handlersResult.status).send(handlersResult);
+});
+
+driverRouter.get('/get-user/:id', async (req, res) => {
+    const handlersResult = responseErrorsHandler('get-user', await userController.getUser(req));
+    res.status(handlersResult.status).send(handlersResult);
+});
 
 module.exports = driverRouter;
