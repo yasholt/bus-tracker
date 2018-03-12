c2062e5c74b8df9a56f6ebcaa7b16fc505db042d
ihorovych <karomigo@gmail.com>
2018-03-12 13:10:23 +0200

    refactored routers

--- bus-tracker-server/index.js
+++ bus-tracker-server/index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const adminRouter = require('./app/routers/adminRouter');
-const driverRouter = require('./app/routers/driverRouter');
 require('./app/services/checkConnection');
+
+// set Routers
+require('./app/routers/adminRouter')(app);
+require('./app/routers/driverRouter')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// set Routers
-app.use('/admin', adminRouter);
-app.use('/driver', driverRouter);
-
